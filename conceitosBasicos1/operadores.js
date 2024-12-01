088397838da7173a7ac83c0377b83f6e552a2e41
charlesanderson25 <charles_spfc@hotmail.com>
2024-12-01 14:48:43 -0300

    Update file

--- conceitosBasicos1/operadores.js
+++ conceitosBasicos1/operadores.js
@@ -1,1 +1,40 @@
 // Operadores
+
+// -------------------------------------------------------------------------------------------------------------------------------//
+
+// Operadores Lógicos
+
+let curso = "javaScript";
+let legal = true;
+
+// AND - Ambas expressões devem ser verdadeiras
+
+if (curso === "javaScript" && legal === true) {
+  console.log("O curso é de javaScript e também é legal!");
+} else {
+  console.log("O curso não é de javaScript ou não é legal");
+}
+
+// OR - Apenas uma das expressões deve ser verdadeira
+
+if (curso === "javaScript" || legal === true) {
+  console.log("O curso é de javaScript e também é legal!");
+} else {
+  console.log("O curso não é de javaScript ou não é legal");
+}
+
+// NOT - Quando queremos fazer uma negação ou verificar se algo não é como esperado
+
+if (curso != "javaScript") {
+  console.log("O curso não é de Javascript");
+} else {
+  console.log("O curso é de javaScript!");
+}
+
+// -------------------------------------------------------------------------------------------------------------------------------//
+
+// Operadores Relacionais
+
+/*
+
+*/
