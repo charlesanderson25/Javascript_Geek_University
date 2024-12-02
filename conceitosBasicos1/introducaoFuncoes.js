5b578856c87062e446b19a67189d6edcec87dc78
charlesanderson25 <charles_spfc@hotmail.com>
2024-12-01 21:40:07 -0300

    Introdução à funções

---
+++ conceitosBasicos1/introducaoFuncoes.js
@@ -0,0 +1,26 @@
+// Introdução à Funções
+
+function soma(num1, num2) {
+  res = num1 + num2;
+
+  return res;
+}
+
+resultado = soma(999, 1);
+console.log(resultado);
+
+//----------------------------------------------------------------------------
+
+function mensagem() {
+  console.log("Sou uma mensagem!");
+}
+
+mensagem();
+
+//----------------------------------------------------------------------------
+
+function multiplicar(n1, n2) {
+  return n1 * n2;
+}
+
+console.log(multiplicar(4, 2));
