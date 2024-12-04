0d4732999205143733daf2652c75aa3909ad5350
charlesanderson25 <charles_spfc@hotmail.com>
2024-12-04 07:32:07 -0300

    Alterando um objeto com forEach

--- conceitosBasicos2/foreach.js
+++ conceitosBasicos2/foreach.js
@@ -15,3 +15,31 @@ let numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 numeros.forEach(function (numero) {
   console.log(`Número - ${numero}`);
 });
+
+console.log("-=".repeat(40));
+
+let numeros1 = [1, 2, 3, 4, 5];
+
+soma = 0;
+
+numeros1.forEach(function (num) {
+  soma = soma + num;
+});
+
+console.log(`A soma entre os elementos do array informado tem valor: ${soma}`);
+
+console.log("-=".repeat(40));
+
+// Alterando um objeto
+
+let alunos = [
+  { aluno: "Charles", idade: 37 },
+  { aluno: "Gécica", idade: 33 },
+  { aluno: "Jade", idade: 3 },
+];
+
+alunos.forEach(function (aluno) {
+  aluno.idade = aluno.idade + 1;
+});
+
+console.log(alunos);
