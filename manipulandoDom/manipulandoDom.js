a7a6445a596aa4a86ab4e5411e3a94bf0ffdcb11
charlesanderson25 <charles_spfc@hotmail.com>
2024-12-08 20:01:07 -0300

    Manipulando a DOM

---
+++ manipulandoDom/manipulandoDom.js
@@ -0,0 +1,10 @@
+let h1 = document.querySelector(".titulo");
+console.log(h1);
+
+console.log(h1.childNodes);
+
+console.log(h1.innerHTML);
+
+console.log(h1.childNodes.childNodes);
+
+console.log(h1.parentNode.parentNode.parentNode);
