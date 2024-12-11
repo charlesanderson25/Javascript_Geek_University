af2a852c8a5098777af3fde49fb5285b4a684ac5
charlesanderson25 <charles_spfc@hotmail.com>
2024-12-11 20:48:34 -0300

    Evento: onmouseover

--- manipulandoDom/eventosInline.js
+++ manipulandoDom/eventosInline.js
@@ -9,3 +9,15 @@ let alteraTitulo = document.querySelector(".titulo");
 
 alteraTitulo.innerText = "Novo título!";
 alteraTitulo.innerHTML = "<h2> Novo Título 2! </h2>";
+
+///////////////////////////// OnMouseOver ////////////////////////////////////
+
+const titulo = document.querySelector(".titulo");
+
+titulo.onmouseover = () => {
+  titulo.innerText = "O mouse está aqui";
+};
+
+titulo.onmouseout = () => {
+  titulo.innerText = "Mouse saiu daqui!";
+};
