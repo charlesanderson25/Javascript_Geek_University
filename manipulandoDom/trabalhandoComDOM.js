0822acbc8ff56ad351c5fe3255ff2e2b6fe804f4
charlesanderson25 <charles_spfc@hotmail.com>
2024-12-25 17:23:45 -0300

    Changes

--- manipulandoDom/trabalhandoComDOM.js
+++ manipulandoDom/trabalhandoComDOM.js
@@ -4,3 +4,15 @@ const spans = document.getElementsByTagName("span");
 
 console.log(spans[0].textContent);
 console.log(spans[1].innerHTML);
+
+//*************************** Captura btn *******************************
+
+let button = document.querySelector("#btn");
+let campoInput = document.querySelector("#campoTexto");
+
+button.addEventListener("click", () => {
+  alert(campoInput.value);
+  console.log(campoInput.value);
+
+  campoInput.value = "";
+});
