6c6e0bf9f886c2c55342cfabbffa8443a92c3e75
charlesanderson25 <charles_spfc@hotmail.com>
2025-01-02 21:39:29 -0300

    Update file

--- javaScriptAssincrono/axios.js
+++ javaScriptAssincrono/axios.js
@@ -1,1 +1,17 @@
 // Utilizando Axios
+
+const axios = require("axios");
+
+let inputAsync = document.querySelector("input[name=github_user]");
+let buttonAsync = document.getElementById("btn1");
+let divAsync = document.getElementById("app");
+
+axios
+  .get(`https://api.github.com/users/${inputAsync.value}`)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.error(error);
+  })
+  .finally(function () {});
