fd5d79c4f97d87ba48b26fdf9aa3593fbc3b4322
charlesanderson25 <charles_spfc@hotmail.com>
2025-01-08 11:52:16 -0300

    Update file

--- javaScriptAssincrono/axios.js
+++ javaScriptAssincrono/axios.js
@@ -10,21 +10,33 @@ buttonAsync.addEventListener("click", () => {
   divAsync.innerHTML = "";
   let textReturn = "";
   let pReturn = document.createElement("p");
+  let imgReturn = document.createElement("img");
 
   axios
     .get(`https://api.github.com/users/${inputAsync.value}`)
     .then(function (response) {
-      if (response.data.name !== null) {
+      console.log("Retorno de response:", response);
+
+      if (response.data.name !== null && response.status === 200) {
         textReturn = document.createTextNode(response.data.name);
         console.log(`O usuário GitHb é: ${textReturn.nodeValue}`);
+
+        imgReturn.setAttribute("src", response.data.avatar_url);
 
         pReturn.appendChild(textReturn);
         console.log(`O valor de pReturn é: ${pReturn.innerHTML}`);
         divAsync.appendChild(pReturn);
+        divAsync.appendChild(imgReturn);
+        inputAsync.value = "";
+      } else {
+        alert(`O usuário ${inputAsync.value} é inválido`);
+        console.log(`O usuário ${inputAsync.value} é inválido`);
       }
     })
     .catch(function (error) {
       console.error(error);
+      alert(`O usuário '${inputAsync.value}' é inválido`);
+      console.log(`O usuário '${inputAsync.value}' é inválido`);
     })
     .finally(function () {});
 });
