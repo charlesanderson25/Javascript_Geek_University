404d00b667eeb959f758bbb7f0f945490ece4de2
charlesanderson25 <charles_spfc@hotmail.com>
2025-01-08 10:35:39 -0300

    Update code

--- javaScriptAssincrono/axios.js
+++ javaScriptAssincrono/axios.js
@@ -1,17 +1,30 @@
 // Utilizando Axios
 
-const axios = require("axios");
+// const axios = require("axios");
 
 let inputAsync = document.querySelector("input[name=github_user]");
 let buttonAsync = document.getElementById("btn1");
 let divAsync = document.getElementById("app");
 
-axios
-  .get(`https://api.github.com/users/${inputAsync.value}`)
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.error(error);
-  })
-  .finally(function () {});
+buttonAsync.addEventListener("click", () => {
+  divAsync.innerHTML = "";
+  let textReturn = "";
+  let pReturn = document.createElement("p");
+
+  axios
+    .get(`https://api.github.com/users/${inputAsync.value}`)
+    .then(function (response) {
+      if (response.data.name !== null) {
+        textReturn = document.createTextNode(response.data.name);
+        console.log(`O usuário GitHb é: ${textReturn.nodeValue}`);
+
+        pReturn.appendChild(textReturn);
+        console.log(`O valor de pReturn é: ${pReturn.innerHTML}`);
+        divAsync.appendChild(pReturn);
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    })
+    .finally(function () {});
+});
